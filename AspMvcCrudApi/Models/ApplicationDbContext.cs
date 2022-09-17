1faf3b5bb810b728dbe2bc6a30717d7f3954ccfe
Pin Phearak <phearakroun@gmail.com>
2022-09-17 13:29:59 +0700

    Asp.net Mvc Crud Api

---
+++ AspMvcCrudApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AspMvcCrudApi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Post> Post { get; set; }
+    }
+}
---
+++ AspMvcCrudApi/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspMvcCrudApi.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Title { get; set; }
+        [Required]
+        public String Body { get; set; }
+
+    }
+}
